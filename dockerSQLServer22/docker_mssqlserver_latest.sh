29fce598c8794ed5b11c51db1223ac2a5c7fe16b
RodrigoKartcheki <rodrigo.kartcheski@gmail.com>
2024-04-27 16:38:59 -0300

    sqlserver

---
+++ dockerSQLServer22/docker_mssqlserver_latest.sh
@@ -0,0 +1,14 @@
+
+# docker rm -f $(docker ps -aq)
+# docker rmi -f $(docker images -q)
+# https://hub.docker.com/_/microsoft-mssql-server
+
+
+# docker pull mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04
+# docker pull mcr.microsoft.com/mssql/server:2022-latest
+
+# docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
+# docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=yourStrong(!)Password" -e "MSSQL_PID=Evaluation" -p 1433:1433  --name sqlpreview --hostname sqlpreview -d mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04
+
+# precisa ter uma rede configurada para rodar o servidor
+# docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=#pigmeu1234" -p 1433:1433 -d --network rede-docker mcr.microsoft.com/mssql/server:2022-latest
