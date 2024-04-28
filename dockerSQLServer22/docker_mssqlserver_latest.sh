ce1a2d6849b7e2e6e116e16a94f81c79c01e6d36
RodrigoKartcheki <rodrigo.kartcheski@gmail.com>
2024-04-28 10:25:33 -0300

    alterações de instrulção

--- dockerSQLServer22/docker_mssqlserver_latest.sh
+++ dockerSQLServer22/docker_mssqlserver_latest.sh
@@ -12,3 +12,8 @@
 
 # precisa ter uma rede configurada para rodar o servidor
 # docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=#pigmeu1234" -p 1433:1433 -d --network rede-docker mcr.microsoft.com/mssql/server:2022-latest
+# docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=#pigmeu1234"-e "MSSQL_PID=Evaluation" -p 1433:1433  --name sqlpreview --hostname sqlpreview -d mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04
+# para acessar a imagem pelo terminal do outro docker
+# sqlcmd -S determined_banach -U sa -P "#pigmeu1234"
+
+
